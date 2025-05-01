3caf621f3b776d2bf3347c7117263b0755efaee2
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-04-30 23:48:30 -0400

    EvenNumbersquared

--- script.js
+++ script.js
@@ -15,5 +15,27 @@ const isEven= (number) => {
     }
 }
 
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Your higher-order function
+const evenNumbersSquared = numbers => {
+// Use filter and/or map
+const evenNumbers = numbers.filter((number) => {
+    if(number % 2 === 0){
+      return true;
+    }
+})
+
+const squaredNumbers = evenNumbers.map((number) => {
+    return number * number;
+   })
+  
+   return squaredNumbers;
+  };
+
+console.log(evenNumbersSquared(numbers));
+
+
+
  
  
