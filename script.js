e63e3b2345609cf212f3aaff319faa6b3e9d36b1
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-04-30 21:56:10 -0400

    arrow functions modidfied

---
+++ script.js
@@ -0,0 +1,19 @@
+const greet = (name) => {
+    return "Hello, " + name + "!";
+}
+ 
+
+const square = (number) => {
+    return number * number;
+}
+
+const isEven= (number) => {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+ 
+ 
